aaffc37462cfd15d41db32bbc71dc2f524d6c306
Adam Rashid <rashid.uj@gmail.com>
2020-09-14 18:47:19 +0800

    Added delete functionality

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ const editButton = document.querySelector('#list-edit-button');
 const title = document.querySelector('h1 span');
 const addButton = document.getElementById('add-item-button');
 const listItem = document.getElementsByClassName('list-item')[0];
+const deleteButton = [...document.getElementsByClassName('delete-button')];
 
 // Set event listeners
 editButton.addEventListener('click', setEditMode);
 title.addEventListener('blur', setReadMode);
 addButton.addEventListener('click', addItem);
+deleteButton.forEach((val, ind, array) => {
+    console.log(val);
+    val.addEventListener('click', deleteItem);
+});
 
 // Callback functions for listeners
 function setEditMode(e) {
     title.contentEditable = true;
     title.style.borderBottom = '1px solid black';
+    title.focus();
 }
 
 function setReadMode(e) {
@@ -21,5 +27,20 @@ function setReadMode(e) {
 }
 
 function addItem(e) {
-    document.getElementsByClassName('list')[0].insertBefore(listItem.cloneNode(true), document.getElementById('add-item-button'));
+    let newItem = listItem.cloneNode(true);
+
+    newItem.querySelector('.item-description').innerHTML = '';
+    newItem.querySelector('.item-description').contentEditable = true;
+
+    // Adding the EventListener, as the EventListener is not automatically attached. Refactor with the below by using event delegation
+    // https://stackoverflow.com/questions/15408394/how-to-copy-a-dom-node-with-event-listeners
+    newItem.querySelector('.delete-button').addEventListener('click', deleteItem);
+    document.getElementsByClassName('list')[0].insertBefore(newItem, document.getElementById('add-item-button'));
+    
+    // Placed after the get Elements By Class Name to allow the focusing to happen after
+    newItem.querySelector('.item-description').focus();
+}
+
+function deleteItem(e) {
+    e.target.parentElement.parentElement.remove();
 }
